d9cb70ef1886dca3fdecafabbd40e22918254bd5
Qasim Mohammmed <37535422+qasimmohammed@users.noreply.github.com>
2024-03-05 11:59:33 +0000

    Adding variables file

---
+++ variables.tf
@@ -0,0 +1,25 @@
+# This variable defines the AWS Region.
+variable "region" {
+  description = "region to use for AWS resources"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "global_prefix" {
+  type    = string
+  default = "demo-msk"
+}
+
+variable "private_cidr_blocks" {
+  type = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+  ]
+}
+
+variable "cidr_blocks_bastion_host" {
+  type = list(string)
+  default = ["10.0.4.0/24"]
+}
